81ffc8af9fb156322ab037c4a1950bc357a3692d
Aleksandar Farkov <alex58020@gmail.com>
2025-10-29 02:42:35 +0200

    Add 'add_thumbnail_to_article' to the tasks module exports

--- backend/news_generation_workers/tasks/__init__.py
+++ backend/news_generation_workers/tasks/__init__.py
@@ -1,9 +1,11 @@
 # Celery tasks package
 from .headline_picker import run_headline_picker_cycle
 from .article_generation import generate_article_from_headline
+from .thumbnail_picker import add_thumbnail_to_article
 
 __all__ = [
     'run_headline_picker_cycle',
     'generate_article_from_headline',
+    'add_thumbnail_to_article',
 ]
 
